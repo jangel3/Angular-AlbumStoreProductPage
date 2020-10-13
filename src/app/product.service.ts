8d6b7309f86422d1d1185fd9b31c82d98ef978ae
John Angel <jangel3@gmail.com>
2020-10-12 23:20:34 -0500

    Product service returns album. Render json on page.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http'
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = "../assets/album.json";
 
-  constructor(private _http: Http) {
+  constructor(private _http: Http, private response: Response) {
 
   }
 
+  getAlbum(id: number)
+  {
+
+    return this._http.get(this._albumUrl).map(this.response.json());
+  }
 }

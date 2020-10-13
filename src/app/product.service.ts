2e9f8a7580ecbab263a7a55e2147f1918c9862ee
John Angel <jangel3@gmail.com>
2020-10-12 23:25:13 -0500

    Corrected response call.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response } from '@angular/http/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = "../assets/album.json";
+  private response: Response;
 
-  constructor(private _http: Http, private response: Response) {
+  constructor(private _http: Http) {
 
   }
 
